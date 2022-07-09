766e2a69491cec2a4b022d51b53a9f4204ae00b6
mksya <mik.sam@hotmail.fr>
2022-07-09 17:45:32 +0200

    angular catalog

---
+++ src/app/new-product/new-product.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-new-product',
+  templateUrl: './new-product.component.html',
+  styleUrls: ['./new-product.component.css']
+})
+export class NewProductComponent implements OnInit {
+
+  productFormGroup!: FormGroup;
+
+  constructor(private fb:FormBuilder, public prodService:ProductService) { }
+
+  ngOnInit(): void {
+    this.productFormGroup=this.fb.group({
+      name : this.fb.control(null,[Validators.required,Validators.minLength(4)]),
+      price : this.fb.control(null,[Validators.required]),
+      promotion : this.fb.control(false,[Validators.required]),
+    });
+  }
+
+  handleAddProduct(){
+    //console.log(this.productFormGroup.value);
+    let product=this.productFormGroup.value;
+    this.prodService.addNewProduct(product).subscribe({
+      next : (data)=>{
+        alert("Products added successfully");
+        this.productFormGroup.reset();
+      },error:err=>{
+        console.log(err);
+      }
+    });
+  }
+
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+import { UUID } from 'angular2-uuid';
+import { Observable, of, throwError } from 'rxjs';
+import { PageProduct, Product } from '../model/product.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  [x: string]: any;
+ 
+  private products! : Array<Product>;
+
+  constructor() { 
+    this.products=[
+      {id:UUID.UUID(),name:"Matcha",price:20,promotion:false},
+      {id:UUID.UUID(),name:"Sencha",price:15,promotion:true},
+      {id:UUID.UUID(),name:"Russian Earl Grey",price:10,promotion:true}
+    ];
+
+    for(let i=0;i<10;i++){
+      this.products.push({id:UUID.UUID(),name:"Matcha",price:20,promotion:false});
+      this.products.push({id:UUID.UUID(),name:"Sencha",price:15,promotion:true});
+      this.products.push({id:UUID.UUID(),name:"Russian Earl Grey",price:10,promotion:true});
+    }
+  }
+
+public getAllProducts() : Observable<Product[]>{
+  let rnd =Math.random();
+  if(rnd<0.1)return throwError(()=>new Error("Error Test"));
+  else return of(this.products);
+}
+
+public getPageProducts(page:number,size:number) : Observable<PageProduct>{
+  let index = page*size;
+  let totalPages = ~~(this.products.length /size);
+  if(this.products.length % size != 0)
+  totalPages++;
+  let pageProducts=this.products.slice(index,index+size);
+  return of({page:page,size:size,totalPages:totalPages,products:pageProducts})
+}  
+
+
+public deleteProduct(id:string) : Observable<boolean>{
+  this.products.filter(p=>p.id != id);
+  return of(true);
+}
+
+public setPromotion(id:string): Observable<boolean>{
+  let product = this.products.find(p=>p.id==id);
+  if(product !=undefined){
+  product.promotion=!product.promotion;
+    return of(true);
+  }else{
+    return throwError(()=>new Error("product not found"));
+  }
+}
+
+public searchProducts(keyword:string,page:number,size:number) : Observable<PageProduct>{
+  let result = this.products.filter(p=>p.name.includes(keyword));
+  let index = page*size;
+  let totalPages = ~~(result.length /size);
+  if(this.products.length % size != 0)
+  totalPages++;
+  let pageProducts=result.slice(index,index+size);
+  return of({page:page,size:size,totalPages:totalPages,products:pageProducts})
+}
+
+public addNewProduct(product:Product):Observable<Product>{
+  product.id=UUID.UUID();
+  this.products.push(product);
+  return of(product);
+}
+
+public getProduct(id:string):Observable<Product>{
+  let product = this.products.find( p=> p.id==id);
+  if(product==undefined) return throwError(()=>new Error("Product not found"));
+  return of (product);
+}
+
+ public getErrorMessage(fieldName:string,error:ValidationErrors){
+    if(error['required']){
+      return fieldName + " is Required";
+    }else if(error['minlength']){
+      return fieldName + " should have at least " + error['minlength']['requiredLength'] + " Characters"; 
+    }else return "";
+  }
+
+  public updateProduct(product:Product):Observable<Product>{
+    this.products=this.products.map(p=>(p.id==product.id)?product:p);
+    return of(product);
+  }
+
+}
